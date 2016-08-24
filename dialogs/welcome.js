3e1cd3c7d11f28618f6b531446ec0e4e4c61eb54
jyfcrw <jyfcrw@gmail.com>
2016-08-24 23:38:02 +0800

    change welcome dialog

--- dialogs/welcome.js
+++ dialogs/welcome.js
@@ -2,14 +2,16 @@ var builder = require('botbuilder');
 
 module.exports = function(bot) {
 
-    var _s = "您好，我是IN-01，" +
-        "您可以通过与我对话，回答我提出的问题，" +
-        "便可以在我们网站上创建出您的个人专访，" +
-        "并能随时更改您的回答，马上开始对话吧！";
+    var _s = "您可以与我对话，回答我提出的问题，" +
+             "便可以在我们网站上创建出您的个人专访，" +
+             "并能随时更改您专访.";
 
     bot.dialog("/welcome", function (session) {
         session.send(_s);
         session.endDialog();
+
+        // Redirect to main dialog
+        session.beginDialog("/");
     });
 
     return bot;
